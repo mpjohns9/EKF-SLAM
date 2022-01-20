5a2b131b3d3d8487b2661e27ab9059055414467b
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-01-19 17:59:14 -0700

    Added implementation of hpp in cpp

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -2,60 +2,94 @@
 #include <cmath>
 #include "rigid2d.hpp"
 
-using turtlelib::Vector2D;
-using turtlelib::Transform2D;
+using namespace turtlelib;
 
 
 Transform2D::Transform2D()
 {
-    double mat[3][3] = {
-        {1, 0, 0},
-        {0, 1, 0},
-        {0, 0, 1}
-    };
+    x = 0;
+    y = 0; 
+    ang = 0;
 }
 
 Transform2D::Transform2D(Vector2D trans)
 {
-    double mat[3][3] = {
-        {1, 0, trans.x},
-        {0, 1, trans.y},
-        {0, 0, 1}
-    };
+    x = trans.x;
+    y = trans.y;
+    ang = 0;
 }
 
 Transform2D::Transform2D(double radians)
 {
-    double mat[3][3] = {
-        {cos(radians), -sin(radians), 0},
-        {sin(radians), cos(radians), 0},
-        {0, 0, 1}
-    };
+    x = 0;
+    y = 0;
+    ang = radians;
 }
 
 Transform2D::Transform2D(Vector2D trans, double radians)
 {
-    double mat[3][3] = {
-        {cos(radians), -sin(radians), trans.x},
-        {sin(radians), cos(radians), trans.y},
-        {0, 0, 1}
-    };
+    x = trans.x;
+    y = trans.y;
+    ang = radians;
+}
+
+Vector2D Transform2D::operator()(Vector2D v) const 
+{
+    Vector2D out;
+    out.x = cos(ang)*v.x + -sin(ang)*v.y + x;
+    out.y = sin(ang)*v.x + cos(ang)*v.y + y;
+    return(out);
+}
+
+Transform2D Transform2D::inv() const
+{
+    Transform2D T;
+    T.x = -x*cos(ang) - y*sin(ang);
+    T.y = -y*cos(ang) + x*sin(ang);
+    T.ang = -ang;
+
+    return T;
+}
+
+// Transform2D & operator*=(const Transform2D & rhs)
+// {
+//     Transform2D T;
+//     T.x = cos(rhs.ang)*x + -sin(rhs.ang)*y + rhs.x;
+//     T.y = sin(rhs.ang)*x + cos(rhs.ang)*y + rhs.y;
+
+//     return T;
+// }
+
+Vector2D Transform2D::translation() const
+{
+    Vector2D v;
+    v.x = x;
+    v.y = y;
+
+    return v;
+}
+
+double Transform2D::rotation() const
+{
+    return ang;
+}
+
+
+std::ostream & operator<<(std::ostream & os, const Transform2D & tf) 
+{
+    os << "angle (deg.): " << tf.rotation() << " x: " << tf.translation().x << " y: " << tf.translation().y;
+    return os;
 }
 
 int main(void) {
-    double d1 = 1.0;
-    double d2 = 1.0;
+    Vector2D trans;
+    trans.x = 4;
+    trans.y = 5;
 
-    if (turtlelib::almost_equal(d1, d2)) {
-        printf("SUCCESS");
-    }
+    Transform2D T(trans);
+
+    // std::cout << T;
+    
     return(0);
 }
 
-Vector2D Transform2D::operator()(Vector2D v) const
-{
-    double mat;
-    mat = {
-        {1, 0, 0, v.x}
-    }
-}
--- turtlelib/rigid2d.hpp
+++ turtlelib/rigid2d.hpp
@@ -154,7 +154,10 @@ namespace turtlelib
         /// \brief \see operator<<(...) (declared outside this class)
         /// for a description
         friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
-
+    private:
+        double x;
+        double y;
+        double ang;
     };
 
 
