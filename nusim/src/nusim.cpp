02b52655cf5cc9a0a5cf68516cdfac1804202b11
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-01-21 12:29:29 -0600

    Added reset service to reset timestep

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,22 +1,34 @@
 #include "ros/ros.h"
 #include <std_msgs/UInt64.h>
+#include <std_srvs/Trigger.h>
+#include <sensor_msgs/JointState.h>
+
+static int timestep;
+
+bool reset_callback(std_srvs::TriggerRequest & request, std_srvs::TriggerResponse & response)
+{
+    timestep = 0;
+    return true;
+}
 
 int main(int argc, char * argv[])
 {
     static int rate;
-    int timestep = 0;
     ros::init(argc, argv, "nusim");
     ros::NodeHandle nh_prv("~");
+    ros::NodeHandle nh;
     ros::Rate r(nh_prv.getParam("rate", rate));
 
-    ros::Publisher pub = nh_prv.advertise<std_msgs::UInt64>("timestep", rate);
+    ros::Publisher pub_step = nh_prv.advertise<std_msgs::UInt64>("timestep", rate);
+    ros::Publisher pub_joints = nh.advertise<sensor_msgs::JointState>("red/joint_states", rate);
+    ros::ServiceServer reset = nh_prv.advertiseService("reset", reset_callback);
     
-
+    timestep = 0;
     while(ros::ok())
     {
         std_msgs::UInt64 step;
         step.data = timestep;
-        pub.publish(step);
+        pub_step.publish(step);
         timestep++;
         ros::spinOnce();
         r.sleep();
