8d1262b82e9261edaa5f9a07dcef884c0f24cc04
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-01-21 17:45:30 -0600

    Modified marker array loop

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -6,11 +6,18 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include "nusim/Teleport.h"
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <ros/console.h>
 
 static int timestep;
 static int rate;
 static double x_0, y_0, theta_0;
 static double x, y, theta;
+static std::vector<double> obs_x;
+static std::vector<double> obs_y;
+static double radius;
+static double height;
 
 bool reset_callback(std_srvs::TriggerRequest & request, std_srvs::TriggerResponse & response)
 {
@@ -26,6 +33,7 @@ bool teleport_callback(nusim::Teleport::Request & request, nusim::Teleport::Resp
     x = request.x;
     y = request.y;
     theta = request.theta;
+    return true;
 }
 
 int main(int argc, char * argv[])
@@ -34,20 +42,28 @@ int main(int argc, char * argv[])
     ros::init(argc, argv, "nusim");
     ros::NodeHandle nh_prv("~");
     ros::NodeHandle nh;
+    ros::NodeHandle nh_obs("~obstacles");
     ros::Rate r(nh_prv.param("rate", rate, 500));
 
     nh_prv.param("x0", x_0, 0.0);
     nh_prv.param("y0", y_0, 0.0);
     nh_prv.param("theta0", theta_0, 0.0);
 
+    nh_obs.getParam("obs_x", obs_x);
+    nh_obs.getParam("obs_y", obs_y);
+    nh_obs.getParam("radius", radius);
+    nh_obs.getParam("height", height);
+
+    // ROS_DEBUG("hello %f",obs_x[0]);
     x = x_0;
     y = y_0;
     theta = theta_0;
 
     std_msgs::UInt64 step;
 
-    ros::Publisher pub_step = nh_prv.advertise<std_msgs::UInt64>("timestep", rate);
-    ros::Publisher pub_joints = nh.advertise<sensor_msgs::JointState>("red/joint_states", rate);
+    ros::Publisher pub_step = nh_prv.advertise<std_msgs::UInt64>("timestep", 1000);
+    ros::Publisher pub_joints = nh.advertise<sensor_msgs::JointState>("red/joint_states", 1000);
+    ros::Publisher marker_pub = nh_obs.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 1, true);
 
     sensor_msgs::JointState js;
 
@@ -57,10 +73,59 @@ int main(int argc, char * argv[])
     static tf2_ros::TransformBroadcaster br;
     geometry_msgs::TransformStamped transform;
     tf2::Quaternion q;
+
+    visualization_msgs::MarkerArray ma;
+
+    ma.markers.resize(obs_x.size());
+    for (int i;i<obs_x.size();i++)
+    {
+        // visualization_msgs::Marker marker;
+
+        //set header and timestamp
+        ma.markers[i].header.frame_id = "world";
+        ma.markers[i].header.stamp = ros::Time::now();
+
+        //set id diff for each marker
+        ma.markers[i].id = i;
+
+        //set color and action
+        ma.markers[i].type = visualization_msgs::Marker::CYLINDER;
+        ma.markers[i].action = visualization_msgs::Marker::ADD;
+
+        //set pose of marker
+        ma.markers[i].pose.position.x = 1;
+        ma.markers[i].pose.position.y = 1;
+        ma.markers[i].pose.position.z = 0;
+        ma.markers[i].pose.orientation.x = 0;
+        ma.markers[i].pose.orientation.y = 0;
+        ma.markers[i].pose.orientation.z = 0;
+        ma.markers[i].pose.orientation.w = 1;
+
+        //set size
+        ma.markers[i].scale.x = 2*radius;
+        ma.markers[i].scale.y = 2*radius;
+        ma.markers[i].scale.z = height;
+
+
+        //set color
+        ma.markers[i].color.r = 1.0;
+        ma.markers[i].color.g = 0.0;
+        ma.markers[i].color.b = 0.0;
+        ma.markers[i].color.a = 1.0;
+
+        // ma.markers[i].lifetime = ros::Duration();
+
+
+        // ma.markers.push_back(marker);
+    }
+
+    marker_pub.publish(ma);
+
     
     timestep = 0;
     while(ros::ok())
     {
+        ROS_DEBUG("hello");
         transform.header.stamp = ros::Time::now();
 
         transform.header.frame_id = "world";
