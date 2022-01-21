fdadf7876cbb12fb8723fe51b2a783f70b21f1b3
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-01-21 11:39:14 -0600

    Added publisher with timestep topic

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,15 +1,23 @@
 #include "ros/ros.h"
+#include <std_msgs/UInt64.h>
 
 int main(int argc, char * argv[])
 {
     static int rate;
+    int timestep = 0;
     ros::init(argc, argv, "nusim");
     ros::NodeHandle nh_prv("~");
-
     ros::Rate r(nh_prv.getParam("rate", rate));
+
+    ros::Publisher pub = nh_prv.advertise<std_msgs::UInt64>("timestep", rate);
+    
 
     while(ros::ok())
     {
+        std_msgs::UInt64 step;
+        step.data = timestep;
+        pub.publish(step);
+        timestep++;
         ros::spinOnce();
         r.sleep();
     }
