6f62f9f85b6fd71295eb033dd0bef418d4d4de46
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-02-03 19:42:33 -0600

    Added documentation

--- nuturtle_control/src/turtle_interface.cpp
+++ nuturtle_control/src/turtle_interface.cpp
@@ -3,6 +3,14 @@
 
 /// PARAMETERS: 
 ///     ticks_to_rad: radians per encoder tick
+///
+/// SUBSCRIBES: 
+///     vel_sub (geometry_msgs/Twist): subscribes to cmd_vel
+///     sensor_sub (nuturtlebot_msgs/SensorData): subscribes to sensor_data
+///
+/// PUBLISHES:
+///     wheel_pub (nuturtlebot_msgs/WheelCommands): publishes wheel_cmd
+///     joint_pub (sensor_msgs/JointState): publishes joint_states
 
 #include "ros/ros.h"
 #include <sensor_msgs/JointState.h>
@@ -10,23 +18,32 @@
 #include "turtlelib/rigid2d.hpp"
 #include "turtlelib/diff_drive.hpp"
 
+// initialize classes
 turtlelib::diffDrive dd;
 turtlelib::Transform2D tf;
 
+// publishing rate 
 static auto rate = 0;
 
+// robot configuration
 static auto x = 0;
 static auto y = 0;
 static auto theta = 0;
 
+// wheel velocities
 static auto lwheel_vel = 0;
 static auto rwheel_vel = 0;
 
+// wheel positions
 static auto lwheel_pos = 0;
 static auto rwheel_pos = 0;
 
+// initialize ticks to radians conversion
 static auto ticks_to_rad = 0;
 
+/// \brief callback for the cmd_vel subscriber
+/// \param msg - geometry_msgs/Twist message obj
+/// Uses the diffDrive library to convert twist to wheel velocities
 void velCallback(const geometry_msgs::Twist & msg)
 {
     tf::Twist2D V{msg.angular.z, msg.linear.x, msg.linear.y};
@@ -35,6 +52,9 @@ void velCallback(const geometry_msgs::Twist & msg)
     rwheel_vel = vel.r_wheel;
 }
 
+/// \brief callback for the sensor_data subscriber
+/// \param msg - nuturtlebot_msgs/SensorData message obj
+/// Sets wheel positions (in encoder ticks)
 void sensorCallback(const nuturtlebot_msgs::SensorData & msg)
 {
     lwheel_pos = msg.left_encoder;
@@ -48,6 +68,8 @@ int main(int argc, char * argv[])
     ros::NodeHandle nh;
     ros::Rate r(nh_prv.param("rate", rate, 500));
 
+    // get encoder ticks conversion param
+    // if it doesn't exist, throw an error
     if (!nh.hasParam("encoder_ticks_to_rad"))
     {
         ROS_ERROR_STREAM("No param named 'encoder_ticks_to_rad'.");
