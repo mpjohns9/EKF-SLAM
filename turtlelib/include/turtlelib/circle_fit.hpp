e01aeee78b205b07d4bffcab7ef47c7cd7590bf8
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-03-16 23:42:57 -0500

    Added library for circle fitting

--- turtlelib/include/turtlelib/circle_fit.hpp
+++ turtlelib/include/turtlelib/circle_fit.hpp
@@ -5,20 +5,36 @@
 /// \brief Circle fitting library
 
 #include<armadillo>
+#include<tuple>
 
-namespace 
+namespace turtlelib
 {
     class circleFit
     {
     public:
-        std::pair circleFit(std::vector<std::vector<double>> x, std::vector<std::vector<double>> y);
-        std::pair compute_centroid(arma::vec x, arma::vec y);
-        std::pair shift(double cent_x, double cent_y);
+        explicit circleFit(std::vector<double> x, std::vector<double> y);
 
+        std::pair<double, double> compute_centroid();
+        std::pair<arma::vec, arma::vec> shift();
+        arma::vec calc_z();
+        arma::mat z_mat();
+        arma::mat moment_matrix();
+        arma::mat constraint_matrix();
+        arma::mat constraint_matrix_inv();
+        std::tuple<arma::mat, arma::vec, arma::mat> svd(arma::mat X);
+        arma::mat calc_A();
+        std::tuple<double, double, double> fit_circle();
+        arma::vec get_clusterx();
+        arma::vec get_clustery();
+        arma::vec get_clusterx_shifted();
+        arma::vec get_clustery_shifted();
+        arma::vec get_z();
 
     private:
         arma::vec cluster_x;
         arma::vec cluster_y;
+        arma::vec cluster_x_shifted;
+        arma::vec cluster_y_shifted;
         arma::vec z;
     };
 }
--- turtlelib/src/circle_fit.cpp
+++ turtlelib/src/circle_fit.cpp
@@ -1,53 +1,190 @@
 #include <armadillo>
 #include <cmath>
 #include <iostream>
+#include "turtlelib/circle_fit.hpp"
 #include <ros/ros.h>
-// #include <tuple>
+#include <tuple>
 
 /// \file 
 /// \brief Circle fitting implementation
 
 namespace turtlelib
 {
-    circleFit::circleFit(std::vector<std::vector<double>> x, std::vector<std::vector<double>> y)
+    circleFit::circleFit(std::vector<double> x, std::vector<double> y)
     {
-        for (int i=0; i<int(x.size()); i++)
-        {
-            arma::vec x_arm = x.at(i);
-            arma::vec y_arm = y.at(i);
-            arma::join_cols(cluster_x, x_arm);
-            arma::join_cols(cluster_y, y_arm);
-        }
-        arma::vec z(int(x.size()));
+        cluster_x = x;
+        cluster_y = y;
+        cluster_x_shifted = arma::vec(int(cluster_x.size()));
+        cluster_y_shifted = arma::vec(int(cluster_y.size()));
+        
+        
+        z = arma::vec(int(x.size()));
     }
 
-    std::pair circleFit::compute_centroid(arma::vec x, arma::vec y)
+    std::pair<double, double> circleFit::compute_centroid()
     {
-        return std::make_pair(mean(x), mean(y));
+        return std::make_pair(mean(cluster_x), mean(cluster_y));
     }
 
-    std::pair circleFit::shift(double cent_x, double cent_y)
+    std::pair<arma::vec, arma::vec> circleFit::shift()
     {
         arma::vec new_x(int(cluster_x.size()), arma::fill::ones);
-        arma::vec new_y(int(cluster_x.size()), arma::fill::ones);
+        arma::vec new_y(int(cluster_y.size()), arma::fill::ones);
 
-        cluster_x = cluster_x - (new_x*cent_x);
-        cluster_y = cluster_x - (new_x*cent_x);
+        double cent_x = std::get<0>(compute_centroid());
+        double cent_y = std::get<1>(compute_centroid());
 
-        return std::make_pair(cluster_x, cluster_y);
+        cluster_x_shifted = cluster_x - (new_x*cent_x);
+        cluster_y_shifted = cluster_y - (new_y*cent_y);
+
+        return std::make_pair(cluster_x_shifted, cluster_y_shifted);
     }
 
     arma::vec circleFit::calc_z()
     {
         for (int i=0; i<int(z.size()); i++)
         {
-            z.at(i) = pow(cluster_x.at(i), 2) + pow(cluster_y.at(i), 2);
+            z.at(i) = pow(cluster_x_shifted.at(i), 2) + pow(cluster_y_shifted.at(i), 2);
         }
         return z;
     }
 
-    // arma::mat circleFit::z_mat()
-    // {
-        
-    // }
+    arma::mat circleFit::z_mat()
+    {
+        arma::vec ones(int(z.size()), arma::fill::ones);
+        arma::mat Z;
+
+        Z = join_rows(z, cluster_x_shifted, cluster_y_shifted, ones);
+
+        return Z;
+    }
+
+    arma::mat circleFit::moment_matrix()
+    {
+        int n = int(z.size());
+        arma::mat M;
+
+        M = (1/n)*(z_mat().t())*(z_mat());
+        return M;
+    }
+
+    arma::mat circleFit::constraint_matrix()
+    {
+        arma::mat H(4, 4, arma::fill::eye);
+        H(0, 0) = 8.0*(arma::mean(z));
+        H(0, 3) = 2;
+        H(3, 0) = 2;
+        H(3, 3) = 0;
+        return H;
+    }
+
+    arma::mat circleFit::constraint_matrix_inv()
+    {
+        arma::mat H_inv(4, 4, arma::fill::eye);
+        H_inv(0, 0) = 0;
+        H_inv(0, 3) = 0.5;
+        H_inv(3, 0) = 0.5;
+        H_inv(3, 3) = -2*(mean(z));
+        return H_inv;
+    }
+
+    std::tuple<arma::mat, arma::vec, arma::mat> circleFit::svd(arma::mat X)
+    {
+        arma::mat U;
+        arma::vec s;
+        arma::mat V;
+
+        arma::svd(U, s, V, X);
+        return std::make_tuple(U, s, V);
+    }
+
+    arma::mat circleFit::calc_A()
+    {
+        arma::mat U;
+        arma::vec s;
+        arma::mat V;
+        arma::vec A;
+        std::tie(U, s, V) = svd(z_mat());
+
+        if (s.back() < pow(10, -12))
+        {
+            A = V.col(3);
+        }
+        else
+        {
+            arma::mat s_mat = arma::diagmat(s);
+            // s_mat.print();
+            arma::mat Y = V*s_mat*V.t();
+            // Y.print();
+            arma::mat Q = Y*constraint_matrix_inv()*Y;
+            // Q.print();
+
+            arma::vec eigval;
+            arma::mat eigvec;
+
+            arma::vec A_star;
+
+            arma::eig_sym(eigval, eigvec, Q);
+
+            for (int i=0; i<int(eigval.size()); i++)
+            {
+                if (eigval.at(i) > 0)
+                {
+                    A_star = eigvec.col(i);
+                    break;
+                }
+            }
+
+            A = arma::solve(Y, A_star);
+        }
+        // A.print();
+        // std::cout << A.at(0);
+        // std::cout << A.at(1);
+        return A;
+    }
+
+    std::tuple<double, double, double> circleFit::fit_circle()
+    {
+        shift();
+        calc_z();
+        arma::vec A = calc_A();
+
+        double a = (-A.at(1))/(2*A.at(0));
+        double b = (-A.at(2))/(2*A.at(0));
+        std::cout << a;
+        std::cout << b;
+        double r = sqrt((pow(A.at(1), 2) + pow(A.at(2), 2) - (4*A.at(0)*A.at(3)))/(4*pow(A.at(0), 2)));
+
+        double c_x = a + arma::mean(cluster_x);
+        double c_y = b + arma::mean(cluster_y);
+
+        return std::make_tuple(c_x, c_y, r);
+    }
+
+    arma::vec circleFit::get_clusterx()
+    {
+        return cluster_x;
+    }
+
+    arma::vec circleFit::get_clustery()
+    {
+        return cluster_y;
+    }
+
+    arma::vec circleFit::get_clusterx_shifted()
+    {
+        return cluster_x;
+    }
+
+    arma::vec circleFit::get_clustery_shifted()
+    {
+        return cluster_y;
+    }
+
+    arma::vec circleFit::get_z()
+    {
+        return z;
+    }
+
+
 }
