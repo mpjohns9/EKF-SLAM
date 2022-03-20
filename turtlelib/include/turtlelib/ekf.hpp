9966e2c3112fb5bee6980e5823d8a7391edfce39
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-03-19 22:58:06 -0500

    Added documentation

--- turtlelib/include/turtlelib/ekf.hpp
+++ turtlelib/include/turtlelib/ekf.hpp
@@ -13,32 +13,71 @@ namespace turtlelib
     {
     public:
 
+        /// \brief initialize EKF variables for n landmarks
+        /// \param n - max number of landmarks expected
         explicit EKF(int n);
 
+        /// \brief initialize EKF variables for n landmarks and robot config
+        /// \param n - max number of landmarks expected
+        /// \param s - robot configuration
         explicit EKF(Config s, int n);
 
+        /// \brief initialize landmarks
+        /// \param obs_x - x coordinates of landmarks
+        /// \param obs_y - y coordinates of landmarks
+        /// \return vector containing currently detected obstacles
         arma::vec initialize_landmarks(std::vector<double> obs_x, std::vector<double> obs_y);
 
+        /// \brief calculate range bearing measurements
+        /// \param j - landmark
+        /// \return vector containing range bearing measurements
         arma::vec calc_h(int j);
 
+        /// \brief calculate jacobian of range bearing vector
+        /// \param j - landmark
+        /// \return jacobian matrix of h
         arma::mat calc_H(int j);
 
+        /// \brief calculate A matrix
+        /// \param u - velocity twist
+        /// \return A matrix
         arma::mat calc_A(Twist2D u);
 
+        /// \brief update state estimate
+        /// \param u - velocity twist
+        /// \return vector containing current state estimate
         arma::vec update_state(Twist2D u);
 
+        /// \brief get current obstacles
+        /// \return vector containing map obstacles
         arma::vec map_state();
 
+        /// \brief get covariance 
+        /// \return tuple of covariance matrices
         std::tuple<arma::mat, arma::mat, arma::mat> sigmas();
 
+        /// \brief get robot configuration estimate
+        /// \return robot config from state estimate
         Config config();
 
+        /// \brief extract obstacles from state vector
+        /// \return obstacle vector
         std::vector<double>  obs_vec();
 
+        /// \brief update estimate using model
+        /// \param u - twist
         void predict(Twist2D u);
         
+        /// \brief update step
+        /// \param j - landmark
+        /// \param x - x coordinate of landmark
+        /// \param y - y coordinate of landmark
         void update(int j, double x, double y);
 
+        /// \brief check known obstacles for data association
+        /// \param x - x coordinate of landmark
+        /// \param y - y coordinate of landmark
+        /// \return true if landmark exists, false if new landmark
         bool check_known_obs(double x, double y);
 
 
